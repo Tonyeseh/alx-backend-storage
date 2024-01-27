17bd1bd57e5648efc01f2d23842252e2e5d94461
Tonyeseh <esehtony123@gmail.com>
2024-01-27 14:05:39 +0100

    task 1 0x02

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,9 @@
 
 import functools
 import redis
-from typing import Callable, Union
+from typing import Callable, Optional, Union
 import uuid
+
 
 def count_calls(method: Callable) -> Callable:
     """wrapper that counts number of call on methods in Cache"""
@@ -15,12 +16,12 @@ def count_calls(method: Callable) -> Callable:
 
         return method(args)
 
-
     return wrapper
 
 
 class Cache:
     """Cache class"""
+
     def __init__(self) -> None:
         """Init Cache class"""
         self._redis = redis.Redis()
@@ -31,3 +32,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]):
+        """get value stored in redis provided the key"""
+        value = self._redis.get(key)
+        if value and fn:
+            value = fn(value)
+
+        return value
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
